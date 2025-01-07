bb8ac461ecbe61c0f67d955c753bab14c50a1d90
sudhakaryadav12 <84092553+sudhakaryadav12@users.noreply.github.com>
2025-01-07 09:07:04 +0530

    Update FileHandlerService.java

--- FileHandlerService.java
+++ FileHandlerService.java
@@ -1,1 +1,10 @@
+import org.springframework.stereotype.Service;
 
+@Service
+public class FileHandlerService {
+
+    public void processFileHandlerRequest(String jsonRequest) {
+        System.out.println("FileHandler JSON Request: " + jsonRequest);
+        // Add logic for handling file-related operations if needed
+    }
+}
